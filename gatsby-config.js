4e1f0c0a8f078eef3d686769ae2c57add9b067f9
Nick <nickeast1998@gmail.com>
2020-09-10 16:06:50 +0100

    image size reduction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Zano Site`,
+    title: `Zano Controls`,
     description: `needs to be added`,
     author: `Nick East`,
     siteUrl: `https://www.example.com`,
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Zano Controls`,
+        short_name: `Zano Controls`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFFFFF`,
+        theme_color: `#FFFFFF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ZANO-LOGO-WHITE.png`,
       },
     },
     {
@@ -53,6 +53,7 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         ssr: true,
+        exclude: ["/404"],
       },
     },
     {
